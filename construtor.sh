1cacba2223663a469bc06ac034bb6c9e526005be
DevTechpy <115493707+DevTechpy@users.noreply.github.com>
2022-10-10 20:29:54 -0300

    Update construtor.sh

--- construtor.sh
+++ construtor.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
 git clone https://edmenn@bitbucket.org/edmenn/gestapp.git
-mv gestapp/*  ./
+mv gestapp/*  /var/www/html/ 
 chmod 777 -R /var/www/html/ 
+
