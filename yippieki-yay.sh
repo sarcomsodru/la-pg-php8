d9f3eb1cedda87f74363986aa9774db19abeb632
DevTechpy <115493707+DevTechpy@users.noreply.github.com>
2022-10-10 19:20:15 -0300

    Update yippieki-yay.sh

--- yippieki-yay.sh
+++ yippieki-yay.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd /opt/
+git clone https://github.com/DevTechpy/la-pg-php8.git
+cd /opt/la-pg-php8/
+cp .env /var/www/html/
 cd /var/www/html/
 git clone https://edmenn@bitbucket.org/edmenn/gestapp.git
-mv gestapp/* 
+mv gestapp/* /var/www/html/ 
 chmod 777 -R /var/www/html/
-cd ~/la-pg-php/
+cd /opt/la-pg-php8/
 docker-compose up -d
